af717768fa2d138a44fc5a67b53b0d5be711e0c8
Enoch18 <32671904+Enoch18@users.noreply.github.com>
2025-03-09 15:28:57 +0200

    changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,6 +15,9 @@
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
+            <p>Email: admin@test.com</p>
+            <p>Password: admin</p>
+
             <div>
                 <x-label for="email" value="{{ __('Email') }}" />
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
